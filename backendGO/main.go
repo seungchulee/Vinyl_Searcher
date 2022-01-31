de998548a30f5ef0dcab6d6e3739b7c917be7dda
SC <inthetelevision@gmail.com>
2022-01-31 17:11:59 +0900

    feat: change api name

--- backendGO/main.go
+++ backendGO/main.go
@@ -11,6 +11,6 @@ import (
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/new", service.GetNewData).Methods("GET")
+	router.HandleFunc("/api/new", service.GetNewData).Methods("GET")
 	log.Fatal(http.ListenAndServe(":1111", router))
 }
